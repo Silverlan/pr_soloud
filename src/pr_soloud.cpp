0ae3bd3382214fa29346361a53507c93df1b065b
Silverlan <silverlann@gmail.com>
2025-07-05 16:34:39 +0200

    chore(license): update from MPL‑2.0 to MIT

--- src/pr_soloud.cpp
+++ src/pr_soloud.cpp
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: (c) 2022 Silverlan <opensource@pragma-engine.com>
+// SPDX-License-Identifier: MIT
+
 #include "pr_soloud.hpp"
 #include <alsoundsystem.hpp>
 #include <alsound_source.hpp>
